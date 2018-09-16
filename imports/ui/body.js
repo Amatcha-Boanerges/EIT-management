4a1ab2f351edb55d9a18559334a8f9866e81ce8d
Amatcha-Boanerges <jacquesyann@live.fr>
2018-09-16 16:23:25 +0000

    3.3  Load eit from EIT collection

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -1,12 +1,11 @@
 import { Template } from 'meteor/templating';
  
+import { EIT } from '../api/eit.js';
+
 import './body.html';
  
 Template.body.helpers({
-  columns: [
-    { text: 'First name' },
-    { text: 'Last name' },
-    { text: 'Gender' },
-    { text: 'Date of birth' },     
-  ],
+    eit() {
+        return EIT.find({});
+      },
 });

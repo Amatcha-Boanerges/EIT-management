7102d1852500e0416a61b8273c0434a4b8fd4139
Amatcha-Boanerges <jacquesyann@live.fr>
2018-09-17 21:34:11 +0000

    If statement for insert a new eit or update an existing eit

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -23,20 +23,34 @@ Template.body.events({
       const Lastname = target.Lastname.value;
       const Gender = target.Gender.value;
       const Dateofbirth = target.Dateofbirth.value;
-   
+      const eitid = target.eitid.value;
+      
+      if (target.eitid.value === '') {
+        EIT.insert({
+          Firstname,
+          Lastname,
+          Gender,
+          Dateofbirth,
+          createdAt: new Date(), // current time
+        });
+      } else {
       // Insert a new eit into the collection
-      EIT.insert({
-        Firstname,
-        Lastname,
-        Gender,
-        Dateofbirth,
-        createdAt: new Date(), // current time
+      EIT.update(target.eitid.value, {
+        $set: { 
+          Firstname: Firstname, 
+          Lastname: Lastname, 
+          Gender: Gender,
+          Dateofbirth: Dateofbirth
+         },
       });
+      }
+      // Create a new method to update a form according to the ID
    
       // Clear form
       target.Firstname.value = '';
       target.Lastname.value = '';
       target.Gender.value = '';
       target.Dateofbirth.value = '';
+      target.eitid.value = '';
     },
   });

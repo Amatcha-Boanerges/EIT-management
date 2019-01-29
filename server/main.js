4e9864ef5a6e2c2522b36ca6ec9314b4cbd3888b
Amatcha-Boanerges <jacquesyann@live.fr>
2019-01-29 13:37:27 +0000

    GraphQl server configuration

--- server/main.js
+++ server/main.js
@@ -1,1 +1,29 @@
-import '../imports/api/eits.js';
+// import '../imports/api/eits.js';
+
+import { WebApp } from 'meteor/webapp'
+import { getUser } from 'meteor/apollo'
+
+import { ApolloServer } from 'apollo-server-express'
+
+import typeDefs from '../imports/api/schema'
+import resolvers from '../imports/api/resolvers'
+
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: async ({ req }) => ({
+    user: await getUser(req.headers.authorization)
+  })
+})
+
+server.applyMiddleware({
+  app: WebApp.connectHandlers,
+  path: '/graphql'
+})
+
+WebApp.connectHandlers.use('/graphql', (req, res) => {
+  if (req.method === 'GET') {
+    res.end()
+  }
+})

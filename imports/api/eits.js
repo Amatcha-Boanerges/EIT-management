e987b63ddbd6c82ef9212fb13dc1689087f39ba2
Amatcha-Boanerges <jacquesyann@live.fr>
2019-01-29 11:26:21 +0000

    Update the file to fit with the graphQL server

--- imports/api/eits.js
+++ imports/api/eits.js
@@ -1,52 +1,52 @@
-import { Meteor } from 'meteor/meteor';
+// import { Meteor } from 'meteor/meteor';
 import { Mongo } from 'meteor/mongo';
-import { check } from 'meteor/check';
+// import { check } from 'meteor/check';
  
 export const EIT = new Mongo.Collection('eit');
 
-// 'click .deleteselected'() { 
-//     EIT.remove({ checked: { $ne: true } });
-//   },
+// // 'click .deleteselected'() { 
+// //     EIT.remove({ checked: { $ne: true } });
+// //   },
 
-Meteor.methods({
-    'eit.insert'(Firstname, Lastname, Gender, Dateofbirth, eitId) {
-      check(Firstname, String);
-      check(Lastname, String);
-      check(Gender, String);
-      check(eitId, String);
+// Meteor.methods({
+//     'eit.insert'(Firstname, Lastname, Gender, Dateofbirth, eitId) {
+//       check(Firstname, String);
+//       check(Lastname, String);
+//       check(Gender, String);
+//       check(eitId, String);
       
-      // Make sure the user is logged in before inserting a task
-      if (! Meteor.userId()) {
-        throw new Meteor.Error('not-authorized');
-      }
+//       // Make sure the user is logged in before inserting a task
+//       if (! Meteor.userId()) {
+//         throw new Meteor.Error('not-authorized');
+//       }
    
-      if (eitId === '') {
-        EIT.insert({
-            Firstname,
-            Lastname,
-            Gender,
-            Dateofbirth,
-            createdAt: new Date(), // current time
-            owner: Meteor.userId(),
-            username: Meteor.user().username,
-          });
-        } else {
-        // Insert a new eit into the collection
-        EIT.update(eitId, {
-          $set: { 
-            Firstname: Firstname, 
-            Lastname: Lastname, 
-            Gender: Gender,
-            Dateofbirth: Dateofbirth,
-            owner: Meteor.userId(),
-            username: Meteor.user().username,
-           },
-        });
-        }
-    },
-    'eit.removeone'(eitId) {
-      check(eitId, String);
+//       if (eitId === '') {
+//         EIT.insert({
+//             Firstname,
+//             Lastname,
+//             Gender,
+//             Dateofbirth,
+//             createdAt: new Date(), // current time
+//             owner: Meteor.userId(),
+//             username: Meteor.user().username,
+//           });
+//         } else {
+//         // Insert a new eit into the collection
+//         EIT.update(eitId, {
+//           $set: { 
+//             Firstname: Firstname, 
+//             Lastname: Lastname, 
+//             Gender: Gender,
+//             Dateofbirth: Dateofbirth,
+//             owner: Meteor.userId(),
+//             username: Meteor.user().username,
+//            },
+//         });
+//         }
+//     },
+//     'eit.removeone'(eitId) {
+//       check(eitId, String);
    
-      EIT.remove(eitId);
-    },
-  });
+//       EIT.remove(eitId);
+//     },
+//   });
